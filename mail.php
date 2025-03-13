7b5795a631010d2dda91f90cbe308a272d0cc0a3
Gustavo Cardona <admin@gustavocardona.com>
2025-03-13 13:40:47 -0700

    Actualización textos formulario al español

--- mail.php
+++ mail.php
@@ -17,10 +17,10 @@ class ContactForm {
 
         if (mail($this->recipient, $subject, $email_content, $email_headers)) {
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Gracias!! Tu mensaje ha sido enviado con exito.";
         } else {
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Lo sentimos! No podemos enviar tu mensaje en estos momentos";
         }
     }
 
@@ -66,12 +66,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         http_response_code(400);
-        echo "Please complete the form and try again.";
+        echo "Por favor completa el formulario e intenta de nuevo.";
         exit;
     }
 
     $contactForm->sendEmail($name, $email, $phone, $subject, $message);
 } else {
     http_response_code(403);
-    echo "There was a problem with your submission, please try again.";
+    echo "Hay un problema con el envío, por favor intenta de nuevo.";
 }
